283afd2f9b6d8831e73999c4f9c6e34b6d2ce0ad
renzocabarios <renzo.cabarios@gmail.com>
2025-03-17 01:48:36 +0800

    Update

--- new/client/src/lib/socket.ts
+++ new/client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const SOCKET = io("http://localhost:3000", {
+export const SOCKET = io("https://cbg-wild-strike.onrender.com", {
     autoConnect: false, // Prevents auto connection on import
     reconnection: false, // Disables auto-reconnection
 });
