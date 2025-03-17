973ddd537abd0546c69bb56e586e68b9eb9c1cdf
renzocabarios <renzo.cabarios@gmail.com>
2025-03-18 06:23:48 +0800

    Update

--- new/client/src/lib/socket.ts
+++ new/client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const SOCKET = io("http://localhost:3000", {
+export const SOCKET = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
     autoConnect: false, // Prevents auto connection on import
     reconnection: false, // Disables auto-reconnection
 });
